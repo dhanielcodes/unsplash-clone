a06a6156d0c19d9d4005e38aa2825a414e5a3257
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-12-15 23:14:32 +0100

    search: got query

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,8 @@
   <div class="hello">
     <div v-for="(item, index) in gns" :key="index">
       <img :src="item.urls.thumb" alt="">
-      {{item.id}}
+       <p>{{item.user.name}}</p>
+      <p>{{item.user.location}}</p>
     </div>
   </div>
 </template>
