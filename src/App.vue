87aa0559c9f06f1c267c017cf3b4ee7265027d46
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-12-15 23:16:14 +0100

    error: fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <router-view></router-view>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
